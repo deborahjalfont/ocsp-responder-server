cd8d8607aa23722487eedee65cc967187aaa8341
deborahjalfont <deborah.jalfon@redislabs.com>
2023-01-01 15:59:06 +0200

    adding necessary packages

--- setup.py
+++ setup.py
@@ -10,12 +10,11 @@ setup(
   author_email='deborah.jalfon@redis.com',
   url='https://github.com/deborahjalfont/ocsp-responder-server',
   keywords=['ocsp', 'responder', 'server'],
-  install_requires=['json-rpc', 'openssl-ocsp-responder'],
+  install_requires=['json-rpc', 'openssl-ocsp-responder', 'werkzeug', 'pyopenssl'],
   classifiers=[
     'Development Status :: 3 - Alpha',
     'Intended Audience :: Developers',
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',
-    'Programming Language :: Python :: 2.7',
   ],
 )
